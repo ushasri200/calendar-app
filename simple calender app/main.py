e99dd77708954c0b07623c96f48bcb044ad4ceaa
Budddha Ushasri <107304039+ushasri200@users.noreply.github.com>
2023-02-15 21:30:56 +0530

    Delete main.py

--- simple calender app/main.py
+++
@@ -1,9 +0,0 @@
-import calendar
-
-#Get the current month
-c_year = int(input("Enter year: "))
-c_month = int(input("Enter month: "))
-
-# Print the calendar
-print(calendar.month(c_year, c_month))
-
