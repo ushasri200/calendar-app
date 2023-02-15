5ec53da0a3af2d6c54495efc3caaf4a49d6f4767
Budddha Ushasri <107304039+ushasri200@users.noreply.github.com>
2023-02-15 21:30:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import calendar
+
+#Get the current month
+c_year = int(input("Enter year: "))
+c_month = int(input("Enter month: "))
+
+# Print the calendar
+print(calendar.month(c_year, c_month))
+
